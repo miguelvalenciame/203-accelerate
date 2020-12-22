fe43a1954e0d3589d26ee369e84621aa81e0df1d
Miguel Valencia <contacto@miguelvalencia.me>
2020-12-21 18:10:40 -0600

    Comitting Accelerate for the first time

--- wp-content/themes/accelerate-theme/index.php
+++ wp-content/themes/accelerate-theme/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -15,29 +16,30 @@
  */
 
 get_header(); ?>
-	<!-- BLOG PAGE -->
-	<section class="index-page">
-		<div class="site-content">
-			<div class="main-content">
-				<?php if ( have_posts() ): ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part('content-blog', get_post_format()); ?>
-					<?php endwhile; ?>
-				<?php endif; ?>
-			</div>
 
-			<?php get_sidebar(); ?>
-		</div>
-	</section>
+<!-- BLOG PAGE -->
+<section class="index-page">
+    <div class="site-content">
+        <div class="main-content">
+            <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+            <?php get_template_part('content-blog', get_post_format()); ?>
+            <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
 
-	<nav id="navigation" class="container">
-		<div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
-		<div class="pagination">
-			<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-				echo 'Page '.$paged.' of '.$wp_query->max_num_pages;
-			?>
-		</div>
-		<div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
-	</nav>
+        <?php get_sidebar(); ?>
+    </div>
+</section>
+
+<nav id="navigation" class="container">
+    <div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
+    <div class="pagination">
+        <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		echo 'Page ' . $paged . ' of ' . $wp_query->max_num_pages;
+		?>
+    </div>
+    <div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
+</nav>
 
 <?php get_footer();
